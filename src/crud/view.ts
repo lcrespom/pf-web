906b9a29abcb644bb3f1ba49eb86515dbf803c8a
Luis Crespo <lcrespo@es.ibm.com>
2017-01-16 13:22:20 +0100

    Working on contact form

--- src/crud/view.ts
+++ src/crud/view.ts
@@ -2,35 +2,45 @@ import H from '../tag-helpers';
 import { Contact, CrudModel, CrudDispatcher } from './types';
 
 
-function viewContacts(model: CrudModel, dispatch: CrudDispatcher) {
-	return H.div([
-		H.button({
-			attrs: { class: 'btn btn-primary' },
-			on: { click: _ => dispatch({ type: 'mode', mode: 'new' })}
-		}, 'New contact'),
-		H.table({
-			attrs: { class: 'table table-hover'} }, [
-			H.thead(
-				H.tr([
-					H.th('Name'), H.th('Surname'), H.th('Company'),
-					H.th('Mobile'), H.th('Phone'), H.th('e-mail')
-				])
-			),
-			H.tbody(model.contacts.map(c =>
-				H.tr([
-					H.td(c.name), H.td(c.surname), H.td(c.company),
-					H.td(c.mobile), H.td(c.phone), H.td(c.email)
-				])
-			))
-		])
+function viewCrudTable(items: any[], fields: string[], labels: string[]) {
+	return H.table('.table.table-hover', [
+		H.thead(
+			H.tr(labels.map(label => H.th(label)))
+		),
+		H.tbody(items.map(item =>
+			H.tr(fields.map(field => H.td(item[field])))
+		))
 	]);
 }
 
+function viewContacts(model: CrudModel, dispatch: CrudDispatcher) {
+	return H.div([
+		H.button('.btn.btn-primary', {
+			on: { click: _ => dispatch({ type: 'mode', mode: 'new' })}
+		}, 'New contact'),
+		viewCrudTable(model.contacts,
+			['name', 'surname', 'company', 'mobile', 'phone', 'email'],
+			['Name', 'Surname', 'Company', 'Mobile', 'Phone', 'e-mail'])
+	]);
+}
+
+/*
+<form class="form-horizontal">
+	<fieldset style="margin-bottom: 10px">
+		<legend>Issuer data</legend>
+
+		<div class="form-group">
+			<!-- xfer date: Date, mandatory, default to today -->
+			<label class="control-label col-sm-3">Transfer date</label>
+			<div class="col-sm-9">
+				<input [(ngModel)]="model.xferDate"
+					type="date" required class="form-control col-sm-9">
+			</div>
+		</div>*/
 function viewContactForm(model: Contact, dispatch: CrudDispatcher) {
 	return H.div([
-		'ToDo: contact form<br>',
-		H.button({
-			attrs: { class: 'btn btn-primary' },
+		//H.br(),
+		H.button('.btn.btn-primary', {
 			on: { click: _ => dispatch({ type: 'mode', mode: 'table' })}
 		}, 'Back to contacts'),
 	]);
