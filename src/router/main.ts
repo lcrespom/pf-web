82a3a72b8cc5282b0826496d6eb8c1b0c5b38ef4
Luis Crespo <lcrespom@gmail.com>
2017-01-22 00:46:41 +0100

    Working on router

--- src/router/main.ts
+++ src/router/main.ts
@@ -1,10 +1,10 @@
-import { runComponent, Dispatcher, VNode } from '../yocto';
+import { runComponent, makeComponent, Dispatcher, VNode } from '../yocto';
 import * as R from 'ramda';
 import H from '../tag-helpers';
 declare const routie;
 
 
-// -------------------- Main app --------------------
+// -------------------- Router --------------------
 
 interface RouteConfig {
 	[route: string]: Function;
@@ -19,59 +19,84 @@ interface RouterModel {
 interface RouterAction {
 	route: string;
 	args: any[];
-};
+}
 
 export type RouterDispatcher = Dispatcher<RouterModel, RouterAction>;
 
 
-function setupRoutes(model: RouterModel, dispatch: RouterDispatcher) {
-	model.routesReady = true;
-	Object.keys(model.routes).forEach(key =>
-		routie(key, (...args) =>
-			dispatch({ route: key, args }))
-	);
+function rInit(routes: RouteConfig): RouterModel {
+	return {
+		routes,
+		routesReady: false,
+		view: _ => H.span()
+	};
 }
 
-function view(model: RouterModel, dispatch: RouterDispatcher): VNode {
+function setupRoutes(model: RouterModel, dispatch: RouterDispatcher) {
+	Object.keys(model.routes).forEach(key =>
+		routie(key, (...args) =>
+			setTimeout(_ => dispatch({ route: key, args }), 0)
+		)
+	);
+	model.routesReady = true;
+}
+
+function rView(model: RouterModel, dispatch: RouterDispatcher): VNode {
 	if (!model.routesReady)
 		setupRoutes(model, dispatch);
+	return model.view(model, dispatch);
+}
+
+function rUpdate(model: RouterModel, action: RouterAction): RouterModel {
+	return R.merge(model, { view: model.routes[action.route] });
+}
+
+export const RouterComponent = makeComponent({
+	init: rInit,
+	view: rView,
+	update: rUpdate
+});
+
+
+// -------------------- Main app --------------------
+
+interface RtrTstModel {}
+
+interface RtrTstAction {}
+
+export type RtrTstDispatcher = Dispatcher<RtrTstModel, RtrTstAction>;
+
+
+function view1(model: RtrTstModel, dispatch: RtrTstDispatcher): VNode {
+	return H.div('Hello from view 1');
+}
+
+function view2(model: RtrTstModel, dispatch: RtrTstDispatcher): VNode {
+	return H.div('Hello from view 2');
+}
+
+function view(model: RtrTstModel, dispatch: RtrTstDispatcher): VNode {
 	const href = url => ({ attrs: { href: url }});
 	return H.div([
 		H.h1('Router'),
 		H.a('.btn.btn-default', href('#route1'), 'Route 1'),
 		'\u00A0\u00A0',
 		H.a('.btn.btn-default', href('#route2'), 'Route 2'),
-		H.div(model.view(model, dispatch))
+		RouterComponent({ props: {
+			route1: view1,
+			route2: view2
+		}})
 	]);
 }
 
-function update(model: RouterModel, action: RouterAction): RouterModel {
-	return R.merge(model, { view: model.routes[action.route] });
+function update(model: RtrTstModel, action: RtrTstAction): RtrTstModel {
+	return model;
 }
 
-function initModel(routes: RouteConfig): RouterModel {
-	return {
-		routes,
-		routesReady: false,
-		view: _ => { throw Error('Router error'); }
-	};
-}
-
-
-function makeView(tag, text) {
-	return function(model, dispatch) {
-		return H[tag](text);
-	};
-}
-
-document.addEventListener('DOMContentLoaded', _ => {
+document.addEventListener('DOMContentLoaded', () => {
 	let container = document.getElementById('route-app');
 	if (!container)
 		throw Error('No element');
-	let routerComponent = { view, update, init: initModel };
-	let routes = {
-		route1: makeView('h3', 'Hello from view 1'),
-		route2: makeView('h3', 'Hello from view 2')
-	};
-	runComponent(routerComponent, container, { props: routes });
+	let rtrTst = { view, update, init: _ => _ };
+	runComponent(rtrTst, container);
 });
