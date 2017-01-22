187f40a6e275ef79c381672db9f0a1c4f0584c74
Luis Crespo <lcrespom@gmail.com>
2017-01-22 09:06:07 +0100

    Simple router componet is ready

--- src/router/main.ts
+++ src/router/main.ts
@@ -1,66 +1,8 @@
-import { runComponent, makeComponent, Dispatcher, VNode } from '../yocto';
 import * as R from 'ramda';
+import { runComponent, Dispatcher, VNode } from '../yocto';
 import H from '../tag-helpers';
-declare const routie;
+import { RouterAction, RouterComponent } from './router';
 
-
-// -------------------- Router --------------------
-
-interface RouteConfig {
-	routes: string[];
-	onRoute: (action: RouterAction) => any;
-}
-
-interface RouterModel {
-	config: RouteConfig;
-	routesReady: boolean;
-}
-
-interface RouterAction {
-	type: 'route';
-	route: string;
-	args: any[];
-}
-
-export type RouterDispatcher = Dispatcher<RouterModel, RouterAction>;
-
-
-function setupRoutes(model: RouterModel, dispatch: RouterDispatcher) {
-	(model.config.routes).forEach(route =>
-		routie(route, (...args) =>
-			setTimeout(_ =>
-				model.config.onRoute({ type: 'route', route, args }),
-			0)
-		)
-	);
-	model.routesReady = true;
-}
-
-function rView(model: RouterModel, dispatch: RouterDispatcher): VNode {
-	if (!model.routesReady)
-		setupRoutes(model, dispatch);
-	return(H.span());
-}
-
-function rUpdate(model: RouterModel, action: RouterAction): RouterModel {
-	return model;
-}
-
-function rInit(config: RouteConfig): RouterModel {
-	return {
-		config,
-		routesReady: false,
-	};
-}
-
-export const RouterComponent = makeComponent({
-	init: rInit,
-	view: rView,
-	update: rUpdate
-});
-
-
-// -------------------- Main app --------------------
 
 interface RtrTstModel {
 	name: string;
